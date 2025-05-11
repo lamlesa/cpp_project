676aa6316339cd716a1bc785216a787256d76220
morozovalesa18@gmail.com <morozovalesa18@gmail.com>
2025-05-11 11:56:41 +0300

    template of structure

---
+++ hunter_class.cpp
@@ -0,0 +1,10 @@
+#include "header_file.hpp"
+
+class Hunter
+{
+    private: int x, y;
+    public:
+    
+    Hunter(int value) : x(value) {}
+
+};
---
+++ starter.cpp
@@ -0,0 +1,6 @@
+#include "header_file.hpp"
+
+int main()
+{
+    return 0;
+}
