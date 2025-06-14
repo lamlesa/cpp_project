9266d24560462f9a2c737a3898f680c78585380f
lamlesa <144051630+lamlesa@users.noreply.github.com>
2025-06-14 13:55:17 +0300

    Delete src/build directory

--- src/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/PROGA/PROJECT/cpp_project/src
-# Build directory: /mnt/c/PROGA/PROJECT/cpp_project/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GameOfLifeCoreTest "/mnt/c/PROGA/PROJECT/cpp_project/src/build/runUnitTests")
-set_tests_properties(GameOfLifeCoreTest PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/PROGA/PROJECT/cpp_project/src/CMakeLists.txt;39;add_test;/mnt/c/PROGA/PROJECT/cpp_project/src/CMakeLists.txt;0;")
